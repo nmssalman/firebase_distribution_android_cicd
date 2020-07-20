734ca4945517c6b5b82cc49e3e4660c64b9e8384
salman <nmssalman@outlook.com>
2020-07-21 02:05:15 +0900

    firebase analytics added

--- app/src/main/java/nmssalman/global/cicdsample/MainActivity.kt
+++ app/src/main/java/nmssalman/global/cicdsample/MainActivity.kt
@@ -2,13 +2,15 @@ package nmssalman.global.cicdsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mFirebaseAnalytics: FirebaseAnalytics
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
         textVersion.setText("Application Version " + BuildConfig.VERSION_NAME)
     }
 }
